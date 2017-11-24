07728a3d138a0c72bb2aee31a8d85335c31ba1cd
zccz14 <zccz14@outlook.com>
2017-11-24 22:26:56 +0800

    Simulator add doc

--- Indicators/Simulator.cs
+++ Indicators/Simulator.cs
@@ -7,31 +7,42 @@ using QuantTC.Indicators.Generic;
 
 namespace QuantTC.Indicators
 {
+	/// <inheritdoc />
     public class Simulator: IIndicator<IBar>
     {
+	    /// <inheritdoc />
 	    public Simulator(Dumb<IBar> source)
 	    {
 		    Source = source;
 		    Source.Update += Resume;
 	    }
-
+		/// <inheritdoc />
 	    public event Action Update;
+	    /// <inheritdoc />
 	    public IEnumerator<IBar> GetEnumerator() => Source.Take(Count).GetEnumerator();
 
+	    /// <inheritdoc />
 	    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+	    /// <inheritdoc />
 	    public int Count { get; private set; }
 
+	    /// <inheritdoc />
 	    public IBar this[int index] => Source[index];
 
 	    private Dumb<IBar> Source { get; }
 
+		/// <summary>
+		/// Replay Source
+		/// </summary>
 	    public void Replay()
 	    {
 		    Count = 0;
 		    Resume();
 	    }
-
+		/// <summary>
+		/// Resume (Continue when source updated)
+		/// </summary>
 	    public void Resume()
 	    {
 		    Functions.Range(Count, Source.Count).ForEach(i =>
