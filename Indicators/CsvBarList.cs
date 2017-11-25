e0fa38bb087699d3ece9544cd5d086f1bc8e8bf6
zccz14 <zccz14@outlook.com>
2017-11-26 01:59:14 +0800

    Disposable CSV Bar List

---
+++ Indicators/CsvBarList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using QuantTC.Data;
+using QuantTC.Indicators.Generic;
+
+namespace QuantTC.Indicators
+{
+	/// <inheritdoc cref="Indicator{T}" />
+	/// <summary>
+	/// Load Bar From CSV File
+	/// 
+	/// [[Datetime, Open, High, Low, Close, Volume, OpenInterest]]
+	/// </summary>
+	public sealed class CsvBarList: Indicator<IBar>, IDisposable
+    {
+	    /// <inheritdoc />
+	    public CsvBarList(string path, bool header = true)
+	    {
+		    Data.AddRange(File.ReadAllLines(path).Skip(header ? 1 : 0).Select(Datum.FromString));
+	    }
+
+	    private class Datum : IBar
+	    {
+		    public DateTime DateTime { get; private set; }
+		    public double Open { get; private set; }
+		    public double High { get; private set; }
+		    public double Low { get; private set; }
+		    public double Close { get; private set; }
+		    public int Volume { get; private set; }
+		    public int OpenInterest { get; private set; }
+
+		    private static Datum FromStringArray(IReadOnlyList<string> data) => new Datum
+		    {
+			    DateTime = DateTime.Parse(data[0]),
+			    Open = double.Parse(data[1]),
+			    High = double.Parse(data[2]),
+			    Low = double.Parse(data[3]),
+			    Close = double.Parse(data[4]),
+			    Volume = int.Parse(data[5]),
+			    OpenInterest = int.Parse(data[6])
+		    };
+
+		    public static Datum FromString(string data) => FromStringArray(data.Split(','));
+	    }
+		/// <inheritdoc />
+		/// <summary>
+		/// Call Follow Up
+		/// </summary>
+	    public void Dispose()
+	    {
+		    FollowUp();
+	    }
+    }
+}
