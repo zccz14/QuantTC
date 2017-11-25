0a089c7082196caea838cf83574966fc2ad75fc8
zccz14 <zccz14@outlook.com>
2017-11-26 02:17:02 +0800

    NetValue Support

---
+++ Indicators/Collectors/NetPosition.cs
@@ -0,0 +1,24 @@
+﻿using QuantTC.Data;
+using QuantTC.Indicators.Generic;
+
+namespace QuantTC.Indicators.Collectors
+{
+	/// <inheritdoc />
+	public class NetPosition : Indicator<int>
+	{
+		/// <inheritdoc />
+		public NetPosition(IIndicator<IOrder> orders)
+		{
+			Orders = orders;
+			Orders.Update += OrdersOnUpdate;
+		}
+
+		private void OrdersOnUpdate()
+		{
+			Data.FillRange(Count, Orders.Count, i => (i > 0 ? Data[i - 1] : 0) + (Orders[i].Direction == OrderDirection.Buy ? 1 : -1) * Orders[i].Lots);
+			FollowUp();
+		}
+
+		private IIndicator<IOrder> Orders { get; }
+	}
+}
---
+++ Indicators/Collectors/NetValue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using QuantTC.Data;
+using QuantTC.Indicators.Generic;
+
+namespace QuantTC.Indicators.Collectors
+{
+	/// <inheritdoc />
+	public class NetValue : Indicator<double>
+	{
+		/// <inheritdoc />
+		public NetValue(IIndicator<IOrder> orders, IIndicator<int> netPosition, IIndicator<double> cash)
+		{
+			Orders = orders;
+			NetPosition = netPosition;
+			Cash = cash;
+			Orders.Update += OrdersOnUpdate;
+		}
+
+		private void OrdersOnUpdate()
+		{
+			Data.FillRange(Count, Orders.Count, i =>
+			{
+				if (i == 0) return 0;
+				if (NetPosition[i - 1] == 0 || NetPosition[i].IsSameSigned(NetPosition[i - 1]) && NetPosition[i].Abs() > NetPosition[i - 1].Abs())
+				{
+					return Data[i - 1]; // yield 
+				}
+				return Cash[i];
+			});
+		}
+
+		private IIndicator<IOrder> Orders { get; }
+		private IIndicator<int> NetPosition { get; }
+		private IIndicator<double> Cash { get; }
+	}
+}
