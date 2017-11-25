0065cb96725c3dbe0ca38aaa6324629a49b533a6
zccz14 <zccz14@outlook.com>
2017-11-26 00:40:38 +0800

    Order Agent

---
+++ Data/IOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuantTC.Data
+{
+    public interface IOrder
+    {
+		DateTime DateTime { get; }
+		double Price { get; }
+		int Lots { get; }
+		OrderDirection Direction { get; }
+    }
+
+	public enum OrderDirection
+	{
+		Buy, Sell
+	}
+}
---
+++ Indicators/Collectors/OrderAgent.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using QuantTC.Data;
+using QuantTC.Indicators.Generic;
+
+namespace QuantTC.Indicators.Collectors
+{
+	/// <inheritdoc />
+	public class OrderAgent : Indicator<IOrder>
+	{
+		/// <inheritdoc />
+		public OrderAgent(IIndicator<IBar> market, IIndicator<int> model)
+		{
+			Market = market;
+			Model = model;
+			Market.Update += OnMarket;
+		}
+
+		private class Datum : IOrder
+		{
+			public DateTime DateTime { get; set; }
+			public double Price { get; set; }
+			public int Lots { get; set; }
+			public OrderDirection Direction { get; set; }
+		}
+
+		private void OnMarket()
+		{
+			if (TargetPosition == Position) return;
+			if (TargetPosition > Position)
+			{
+				Data.Add(new Datum{DateTime = Market.Last().DateTime, Lots = TargetPosition - Position, Price = Market.Last().Open, Direction = OrderDirection.Buy});
+			}
+			else
+			{
+				Data.Add(new Datum { DateTime = Market.Last().DateTime, Lots = Position - TargetPosition, Price = Market.Last().Open, Direction = OrderDirection.Sell});
+			}
+			Position = TargetPosition;
+			FollowUp();
+		}
+		/// <summary>
+		/// Target Position: The last position that the model indicated
+		/// </summary>
+		public int TargetPosition => Model.LastOrDefault();
+		/// <summary>
+		/// Current Position: The Order Agent has ordered
+		/// </summary>
+		public int Position { get; private set; }
+
+		private IIndicator<IBar> Market { get; }
+		private IIndicator<int> Model { get; }
+	}
+}
