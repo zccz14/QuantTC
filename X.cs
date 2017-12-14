8e711b0f38c32f9074648db710bb644aabae0ad4
zccz14 <zccz14@outlook.com>
2017-12-14 14:25:23 +0800

    Extend IReadOnlyList with NearPairAt method

---
+++ X.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantTC
+{
+    public static class X
+    {
+        /// <summary>
+        /// VT(src[idx - 1], src[idx])
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static Tuple<T, T> NearPairAt<T>(this IReadOnlyList<T> source, int index) =>
+            Tuple.Create(source[index - 1], source[index]);
+    }
+}
