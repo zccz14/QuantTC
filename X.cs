97d24d960b6c3f25aeff10afc9c6ce12e3eac262
zccz14 <zccz14@outlook.com>
2017-12-15 13:47:30 +0800

    Apply

--- X.cs
+++ X.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace QuantTC
 {
-    public static class X
+    public static partial class X
     {
         /// <summary>
         /// VT(src[idx - 1], src[idx])
@@ -15,9 +15,6 @@ namespace QuantTC
         /// <returns></returns>
         public static Tuple<T, T> NearPairAt<T>(this IReadOnlyList<T> source, int index) =>
             Tuple.Create(source[index - 1], source[index]);
-
-        public static TR Apply<T1, TR>(this T1 arg, Func<T1, TR> func) => func(arg);
-        public static TR Apply<T1, T2, TR>(this T1 arg, Func<T1, T2, TR> func, T2 arg2) => func(arg, arg2);
 
         public static IEnumerable<int> ToRange(this Tuple<int, int> range) => Functions.Range(range.Item1, range.Item2);
 
