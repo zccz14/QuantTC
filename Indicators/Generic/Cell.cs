da0706dff60142482bf4ceadf64f4138eb16755f
zccz14 <zccz14@outlook.com>
2018-02-13 17:07:54 +0800

    Tracking and Cell

---
+++ Indicators/Generic/Cell.cs
@@ -0,0 +1,36 @@
+﻿namespace QuantTC.Indicators.Generic
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Cell is a wrapped state
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <remarks>
+    /// The state inside a cell can be changed by outside.
+    /// It will be saved in a list when the timer updates.
+    /// If type T is a value type, a value-copy will occur.
+    /// If type T is a reference type, it will only copy its reference, which may introduce 'future function'.
+    /// </remarks>
+    public class Cell<T> : Indicator<T>
+    {
+        /// <inheritdoc />
+        public Cell(IIndicator timer, T initValue = default(T))
+        {
+            Timer = timer;
+            Value = initValue;
+            Timer.Update += TimerOnUpdate;
+        }
+
+        private void TimerOnUpdate()
+        {
+            Data.Add(Value);
+            FollowUp();
+        }
+
+        private IIndicator Timer { get; }
+        /// <summary>
+        /// the value of the cell
+        /// </summary>
+        public T Value { get; set; }
+    }
+}
---
+++ Indicators/Generic/Tracking.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace QuantTC.Indicators.Generic
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Tracking a quantity automatically when timer updated.
+    /// </summary>
+    /// <typeparam name="T">Quantity Type</typeparam>
+    /// <remarks>
+    /// There's no granted that the count is equal to the timer.
+    /// </remarks>
+    public class Tracking<T>: Indicator<T>
+    {
+        /// <inheritdoc />
+        public Tracking(IIndicator timer, Func<T> getter)
+        {
+            Timer = timer;
+            Getter = getter;
+            Timer.Update += Timer_Update;
+        }
+
+        private void Timer_Update()
+        {
+            Data.Add(Getter());
+            FollowUp();
+        }
+
+        private IIndicator Timer { get; }
+        private Func<T> Getter { get; }
+    }
+
+    public static partial class X
+    {
+        /// <summary>
+        /// Create a tracking instance
+        /// </summary>
+        /// <typeparam name="T">Quantity Type</typeparam>
+        /// <param name="This">Update Timer</param>
+        /// <param name="getter"></param>
+        /// <returns></returns>
+        public static Tracking<T> Track<T>(this IIndicator This, Func<T> getter) =>
+            new Tracking<T>(This, getter);
+
+        /// <summary>
+        /// Create a tracking instance, shortcut for This.Track(that.LastOrDefault)
+        /// </summary>
+        /// <typeparam name="T">Quantity Type</typeparam>
+        /// <param name="This">Update Timer</param>
+        /// <param name="that"></param>
+        /// <returns></returns>
+        public static Tracking<T> Track<T>(this IIndicator This, IReadOnlyList<T> that) =>
+            This.Track(that.LastOrDefault);
+    }
+}
